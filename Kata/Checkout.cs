372391ae43b8c9af11c807f47440d15e12bb9913
Julien-Francois <Julien-Francois@DESKTOP-8P2PMVB>
2020-05-16 18:44:03 +0100

    Add project files.

---
+++ Kata/Checkout.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Kata
+{
+    public class Checkout
+    {
+        private List<Item> items = new List<Item>();
+        public decimal Total()
+        {
+            return items.Sum(i => i.Price);
+        }
+
+        public void Scan(Item item)
+        {
+            items.Add(item);
+        }
+    }
+}
---
+++ Kata/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kata
+{
+    public class Item
+    {
+        public string Sku { get; set; }
+        public decimal Price { get; set; }
+    }
+}
