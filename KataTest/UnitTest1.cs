0d70f7bdf6de544b8255fb40731503355d18728e
Julien-Francois <Julien-Francois@DESKTOP-8P2PMVB>
2020-05-16 18:51:17 +0100

    Adding test library

---
+++ KataTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Kata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ComponentModel.Design;
+
+namespace KataTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
