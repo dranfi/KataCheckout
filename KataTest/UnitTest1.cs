73329db1334002f5e87124f6e3bfb627f7fa4c45
Julien-Francois <Julien-Francois@DESKTOP-8P2PMVB>
2020-05-16 19:00:38 +0100

    First tests

--- KataTest/UnitTest1.cs
+++ KataTest/UnitTest1.cs
@@ -7,10 +7,26 @@ namespace KataTest
     [TestClass]
     public class UnitTest1
     {
+        Item item1 = new Item() { Sku = "A99", Price = 0.50m };
+        Item item2 = new Item() { Sku = "B15", Price = 0.30m };
+        Item item3 = new Item() { Sku = "C40", Price = 0.60m };
         [TestMethod]
-        public void TestMethod1()
+        public void TotalPriceOneItem()
         {
-            
+            var checkout = new Checkout();
+            checkout.Scan(item1);
+            var total = checkout.Total();
+            Assert.AreEqual(0.50m, total);
+        }
+        [TestMethod]
+        public void TotalPriceThreeItem()
+        {
+            var checkout = new Checkout();
+            checkout.Scan(item1);
+            checkout.Scan(item2);
+            checkout.Scan(item3);
+            var total = checkout.Total();
+            Assert.AreEqual(1.40m, total);
         }
     }
 }
